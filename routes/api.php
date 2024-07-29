600bc60b1b1b9ee997a2410ada29c29fba4e8264
trozaye <terryoppman@gmail.com>
2024-07-29 11:17:49 +0000

    api endpoints1

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\TaskController;
+use App\Http\Controllers\CategoryController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -13,6 +15,31 @@ Route::post('/register', [AuthController::class, 'register']);
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::apiResource('tasks', TaskController::class);
-    Route::apiResource('categories', CategoryController::class);
+    // Get all tasks
+    Route::get('tasks', [TaskController::class, 'index']);
+    
+    // Get a specific task
+    Route::get('tasks/{task}', [TaskController::class, 'show']);
+    
+    // Create a new task
+    Route::post('tasks', [TaskController::class, 'store']);
+    
+    // Update a specific task
+    Route::put('tasks/{task}', [TaskController::class, 'update']);
+    
+    // Delete a specific task
+    Route::delete('tasks/{task}', [TaskController::class, 'destroy']);
+});
+
+Route::middleware('auth:sanctum')->group(function () {
+
+    Route::get('categories', [CategoryController::class, 'index']);
+    
+    Route::get('categories/{category}', [CategoryController::class, 'show']);
+    
+    Route::post('categories', [CategoryController::class, 'store']);
+   
+    Route::put('categories/{category}', [CategoryController::class, 'update']);
+   
+    Route::delete('categories/{category}', [CategoryController::class, 'destroy']);
 });
